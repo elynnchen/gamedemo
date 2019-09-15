f582113eeff43feac475b97fc31ff1c441353743
elynnchen <31947637+elynnchen@users.noreply.github.com>
2019-09-15 22:26:33 +0800

    aaaa
    aaaaaa

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+  {{n}}
+  <div class="row">
+     <Cell v-on:click="onClickCell(0,$event)" v-bind:n='n'></Cell>
+     <Cell v-on:click="onClickCell(1,$event)" v-bind:n='n'></Cell>
+     <Cell v-on:click="onClickCell(2,$event)" v-bind:n='n'></Cell>
+  </div>
+  <div class="row">
+      <Cell @click="onClickCell(3,$event)" :n='n'></Cell>
+     <Cell @click="onClickCell(4,$event)" :n='n'></Cell>
+     <Cell @click="onClickCell(5,$event)" :n='n'></Cell>
+  </div>
+  <div class="row">
+     <Cell @click="onClickCell(6,$event)" :n='n'></Cell> 
+     <Cell @click="onClickCell(7,$event)" :n='n'></Cell>
+     <Cell @click="onClickCell(8,$event)" :n='n'></Cell>
+  </div>
+
+
+</div>
+ 
+</template>
+
+<script>
+import Cell from './components/cell'
+
+export default {
+  name: 'app',
+  components: { Cell},
+  data(){
+    return{n:0}
+  },
+  methods:{
+    onClickCell(i,b){
+      this.n=this.n+1;
+     console.log(this.n)
+  }
+  }
+
+}
+
+</script>
+
+<style>
+
+.row{
+  display:flex;
+
+}
+</style>
---
+++ src/components/cell.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app" > 
+   {{n}}
+    <div class="cell" @click="onClickMe">
+    <div v-if="a"> {{b}}</div>
+    <div v-else></div>
+    </div>
+ 
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  props:["n"],
+  data(){
+    return{a:false , b:""}
+  },
+ 
+  methods:{
+    onClickMe(){
+      if(this.b!==""){
+        return
+      }//不能重复点击，如果不为空证明点过了
+      this.a = true;
+      this.b=this.n %2==0?'x':'O';//奇数为O，偶数为X；
+      this.$emit('click')
+    }
+  }
+
+}
+</script>
+
+<style>
+.cell{
+  border:1px solid #333;
+  width:100px;
+  height:100px;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+}
+</style>
