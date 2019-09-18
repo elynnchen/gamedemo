32750d6d97ac1cfaefe54cbedd14170a0a455444
elynnchen <44544738@qq.com>
2019-09-18 11:06:49 +0800

    添加再来一次按钮

--- src/demo/game-0.vue
+++ src/demo/game-0.vue
@@ -3,8 +3,8 @@
   {{n}}
   <div class="row">
      <Cell v-on:click="onClickCell(0,$event)" v-bind:n='n'></Cell>
-     <Cell v-on:click="onClickCell(1,$event)" v-bind:n='n'></Cell>
-     <Cell v-on:click="onClickCell(2,$event)" v-bind:n='n'></Cell>
+     <Cell v-on:click="onClickCell(1,$event)" v-bind:n='n' ></Cell>
+     <Cell v-on:click="onClickCell(2,$event)" v-bind:n='n' ></Cell>
   </div>
   <div class="row">
      <Cell @click="onClickCell(3,$event)" :n='n'></Cell>
@@ -12,7 +12,7 @@
      <Cell @click="onClickCell(5,$event)" :n='n'></Cell>
   </div>
   <div class="row">
-     <Cell @click="onClickCell(6,$event)" :n='n'></Cell> 
+     <Cell @click="onClickCell(6,$event)" :n='n'></Cell>
      <Cell @click="onClickCell(7,$event)" :n='n'></Cell>
      <Cell @click="onClickCell(8,$event)" :n='n'></Cell>
   </div>
@@ -41,16 +41,40 @@ export default {
       }
   },
   methods:{
+      onClickClear() {
+          // location.reload()
+          /*   this.$nextTick(function () {
+                 console.log("1111111");
+                 for(let i=0;i<=2;i++){
+                     for(let j=0;j<=2;j++){
+                         // 更新数据
+                         this.$set(this.map[i], j, null);
+                     }
+                 }
+             });*/
+          for(let i=0;i<=2;i++){
+              for(let j=0;j<=2;j++){
+                  //this.$refs.child.$data.b=null;
+                  //this.$children[i]=null;
+               // this.$set(this.map[i], j,this.$refs.child.$data.b )
+                  for(let i=0;i<this.$children.length;i++){
+                      console.log(this.$children[i].$data.b);
+                      this.$children[i].$data.b="";
+                  }
+                  this.$set(this.map[i], j,this.$children[i].$data.b )
+              }
+          }
+      },
     onClickCell(i, b){//i表示序号，被点击，但不知道是X还是O，子组件需Emit内容出来
-    console.log(`${i}被点击,内容是${b}`);
-    this.map[Math.floor(i/3)][i%3]=b;
-    this.n=this.n+1;
-    this.tell()
+        console.log(`${i}被点击,内容是${b}`);
+        this.map[Math.floor(i/3)][i%3]=b;
+        this.n=this.n+1;
+        this.tell()
   },
       tell(){
           let map=this.map;
           for(let i=0; i<2;i++){
-              if( map[i][0]!==null &&
+              if( map[i][0]!==null && map[i][0]!=="" &&
                   map[i][0]===map[i][1] &&
                   map[i][1]===map[i][2]){
                   this.result=true;
@@ -58,7 +82,7 @@ export default {
           }
           for(let j=0; j<2;j++){
               if(
-                  map[0][j]!==null&&
+                  map[0][j]!==null&&map[0][j]!==""&&
                   map[0][j]===map[1][j]&&
                   map[1][j]===map[2][j]){
                   this.result=true;
@@ -66,7 +90,7 @@ export default {
           }
           for(let i=0; i<2;i++){
               if(
-                  map[i][i]!==null&&
+                  map[i][i]!==null&&map[i][i]!==""&&
                   map[0][0]===map[1][1]&&
                   map[1][1]===map[2][2]){
                   this.result=true;
@@ -75,29 +99,13 @@ export default {
           }
           for(let i=0; i<2;i++){
               if(
-                  map[0][2]!==null&&
+                  map[0][2]!==null&&map[0][2]!==""&&
                   map[0][2]===map[1][1]&&
                   map[1][1]===map[2][0]){
                   this.result=true;
               }
           }
       },
-      onClickClear() {
-         // location.reload()
-          this.$nextTick(function () {
-              console.log("1111111");
-              for(let i=0;i<=2;i++){
-                  for(let j=0;j<=2;j++){
-                      // 更新数据
-                      this.$set(this.map[i], j, null)
-
-                  }
-              }
-          });
-
-      },
-
-
   }
 }
 </script>
